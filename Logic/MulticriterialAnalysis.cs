444fb08ffd27e12471bb053b95bd9ae77b70a9f7
Chromigo <Chromigo@gmail.com>
2013-11-14 16:50:48 +0400

    Написал базовую логику алгоритма лса:(пока не работает с реальными данными) 1. Получение начального множества Парето 2. Определение коэффициентов относительной важности(из захардкоженых значений важности одних коэффициентов, в сравнении с другими) 3. Пересчет наименее важных критериев(предполагая что наши критерии подчиняются закону критериA<критерийБ<критерийВ<...). Возможны более сложные варианты, но их стоит предварительно обсудить и на основании этого запрогать нужный вариант 4. Построение нового множества Парето

--- Logic/MulticriterialAnalysis.cs
+++ Logic/MulticriterialAnalysis.cs
@@ -8,6 +8,11 @@ namespace Logic
 {
     public class MulticriterialAnalysis
     {
+        /// <summary>
+        /// Строит множество Парето из заданного множества
+        /// </summary>
+        /// <param name="educationLinesAndRequirementTable">Множество, из которого требуется строить мн-во Парето</param>
+        /// <returns>Построенное множество Парето</returns>
         public List<EducationLineAndRequirementRow> ParretoSetCreate(List<EducationLineAndRequirementRow> educationLinesAndRequirementTable)
         {
             List<EducationLineAndRequirementRow> parretoList = new List<EducationLineAndRequirementRow>();
@@ -70,6 +75,21 @@ namespace Logic
             }
 
             return parretoList;
+        }
+
+        /// <summary>
+        /// Вычисяет коэффициент относительной важности для 2-х критериев(i-й и j-й)
+        /// </summary>
+        /// <param name="wi">Сколько единиц ЛПР хочет получить в i-м критерии</param>
+        /// <param name="wj">Сколькими единиц ЛПР готово пожертвовать в j-м критерии</param>
+        /// <returns>Коэффициент относительной важности</returns>
+        public double GetTeta(int wi, int wj)
+        {
+            var teta = wj / (wi + wj);
+            if (0 < teta && teta < 1)
+                return teta;
+            else 
+                throw new ArithmeticException("Значение коэффициента относительной важности не лежит в пределах 0<teta<1");
         }
     }
 }
